9e831d0e414838ecca7b2a6b02bd74703a7e7af7
kriserohalia <kriserhl907@gmail.com>
2024-06-24 12:01:49 +0700

    ganti commit

--- Main.java
+++ Main.java
@@ -1,43 +1,103 @@
-// Nama : Krise Rohalia
+package tugas_1;
 
-import java.util.Scanner;
+import java.util.*;
+
+// Annur Akbar Bahagia
+// BE(Java) SKSK Batch 23
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+        System.out.println("=========>Mini Program<=========");
+        System.out.println("1. Mini Kalkulator");
+        System.out.println("2. Estimasi Waktu Perjalanan");
+        System.out.print("Pilih Program :");
+        String plih = scanner.next();
 
-        double upahPerBuah = 450;
-        double bonus = 0 ;
+        switch(plih){
+            case "1":{
+                kalkulator calc = new kalkulator();
+                System.out.println("=========>Pilih Oprasi<=========");
+                System.out.println("+");
+                System.out.println("-");
+                System.out.println("/");
+                System.out.println("x");
+                System.out.println("%");
 
-        System.out.print("Nama Pekerja : ");
-        String nama = scanner.nextLine();
+                System.out.print("Oprasi :");
+                String oprasi = scanner.next();
 
-        System.out.print("Jumlah barang selesai : ");
-        int jumlah = scanner.nextInt();
+                System.out.print("Nilai1 :");
+                int nilai = scanner.nextInt();
+                
+                System.out.print("Nilai1 :");
+                int nilai2 = scanner.nextInt(); 
+                System.out.println("Hasil = "+calc.calc(oprasi, nilai, nilai2)); 
+                break;
+            }
+            case "2":{
+                estimasiWaktu estimateTime = new estimasiWaktu();
+                System.out.print("Jarak tempuh :");
+                int jarak = scanner.nextInt();
+                
+                System.out.print("Kecepatan :");
+                int kecepatan = scanner.nextInt(); 
 
-        System.out.print("Apakah Lembur (true / false) ? ");
-        boolean lembur = scanner.nextBoolean();
-
-        double totalUpah = hitungUpah (upahPerBuah, jumlah, lembur, bonus);
-
-        System.out.print("\nNama : " + nama);
-        System.out.print("\nTotal Upah : " + totalUpah);
+                System.out.println("Estimasi Waktu tempuh = " + estimateTime.estimateTime(jarak, kecepatan)+" Jam");
+            }
+        }
 
         scanner.close();
-
-        
     }
+}
 
-    private static double hitungUpah (double upahPerBuah, int jumlah, boolean lembur, double bonus ){
+class kalkulator{
+    private int result;
 
-        if (jumlah >= 100){
-            bonus = (upahPerBuah / 4) * jumlah;
+    // Calculator Function
+    public int calc(String opration,int nilai,int nilai2){
+
+        switch(opration){
+            case "+":{
+                result = nilai+nilai2;
+                break;
+            }
+            case "-":{
+                result = nilai-nilai2;
+                break;
+            }
+            case "/":{
+                result = nilai2 == 0 ? 0 : nilai+nilai2;
+                break;
+            }
+            case "x":{
+                result = nilai*nilai2;
+                break;
+            }
+            case "%":{
+                result = nilai%nilai2;
+                break;
+            }
+            default:{
+                System.out.println("Oprasi Tidak Valid");
+            }
         }
-        double tarifAkhir = lembur ? upahPerBuah  * 2 : upahPerBuah;
-
-        return (jumlah * tarifAkhir + bonus);
-
+        return result;
     }
-    
+}
+
+class estimasiWaktu{
+
+    private float result = 0;
+
+    public float estimateTime(int jarak,int kecepatan) {
+       if(jarak == 0 || kecepatan == 0){
+        System.out.println("input kecepatan dan jarak dengan benar");
+       }else{
+        result = jarak/kecepatan;
+       }
+
+        return result;
+    }
+
 }
