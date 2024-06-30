22b556bc92c03bd169c1fa1478bd8fb9988fd86e
Bagassukma <bagasreyz1620@gmail.com>
2024-06-30 20:51:13 +0700

    tugas ke 3

---
+++ Maven/bagassukma/base/src/main/java/com/sksk/Main.java
@@ -0,0 +1,24 @@
+package com.sksk;
+
+public class Main {
+    public static void main(String[] args){
+        System.err.println("Hello World!");
+
+        String firstName = "Bagas ";
+        String lastName = "Sukma Patria";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+        int x = 16;
+        int y = 20;
+        if (x > y) {
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
+
+        String[] fruit = {"Apel", "Mango", "Grape", "Pear"};
+        fruit[0] = "Banana";
+        System.out.println(fruit[0]);
+    }
+}
